28d126cf58826344ad225226144543f16effa9ab
Gilad Adler <gilad.adler@riverbed.com>
2020-02-12 16:07:53 +0200

    add jenkins file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,67 @@
+import java.text.SimpleDateFormat
+
+node ('docker') {
+
+    deleteDir()
+
+    stage 'get artifacts'
+
+    // get source code
+    checkout scm
+
+    // create the third-party 'lib' folder
+    def mvnHome = tool 'M3'
+    sh "${mvnHome}/bin/mvn -B clean package"
+
+    stage 'build docker'
+
+//    def localRepositoryUrl = "https://${env.DOCKER_LOCAL_REGISTRY}"
+//
+//    sh "shopt -s dotglob && cp docker/* ."
+//
+//    String tupleBuilder = "tuple-builder"
+//    String buildNumber = "2.0." + currentBuild.id
+//    currentBuild.displayName = buildNumber
+//    String tagName = "TupleBuilder_${buildNumber}"
+//
+//    String fullLocalDockerUri = "docker-registry.lab.aternity.com/${tupleBuilder}:${tagName}"
+//    sh "docker login -u admin -p admin ${localRepositoryUrl}"
+//    sh "docker build -t ${fullLocalDockerUri} ."
+//    sh "docker push ${fullLocalDockerUri}"
+
+    stage 'run tupleBuilder app'
+
+//        node('automation-services') {
+//
+//            sh "docker pull docker-registry.lab.aternity.com/${tupleBuilder}:${tagName}"
+//
+//            String result = runShell("docker ps -a | grep ${tupleBuilder}")
+//
+//            if (result.contains("${tupleBuilder}")) { // if service exists remove it
+//                sh "docker rm -f ${tupleBuilder}"
+//            }
+//
+//            // start new servcie
+//            sh script: """ \
+//            docker run -d --name ${tupleBuilder} \
+//            --restart=unless-stopped \
+//            -e TZ=Asia/Jerusalem \
+//            -t --publish 9200:9200 \
+//            docker-registry.lab.aternity.com/${tupleBuilder}:${tagName} \
+//            """
+//
+//            String listServiceOutput = sh(returnStdout: true, script: "docker ps | grep ${tupleBuilder} | grep \"Up \"")
+//            if (!listServiceOutput.contains(tupleBuilder))
+//                error "failed to run ${tupleBuilder} service. see service ls command output."
+//    }
+
+    deleteDir()
+
+}
+
+def runShell(String command) {
+    return sh (
+            script: command + " || true",
+            returnStdout: true
+    ).trim()
+}
